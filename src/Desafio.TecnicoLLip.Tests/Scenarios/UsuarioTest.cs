cdad0de982c811c4a0b81d6c609905dd04e180e7
alexa <alexandro_alves@yahoo.com.br>
2023-05-02 07:59:22 -0300

    Subida Test integração

---
+++ src/Desafio.TecnicoLLip.Tests/Scenarios/UsuarioTest.cs
@@ -0,0 +1,20 @@
+﻿using LlipTests.Scenarios.Base.CRUD;
+using Xunit.Extensions.Ordering;
+
+namespace Desafio.TecnicoLLip.Tests.Scenarios.Gestor.Odontograma
+{
+    [Order(1)]
+    public class UsuarioTest : Operations
+    {
+        public UsuarioTest()
+        {
+            Body = new
+            {
+                Name = "aaaaaaa",
+                Email = "a@a.com.br",
+                Password = "aaaaaaaa"
+            };
+            Configure("Projetos", "1");
+        }
+    }
+}
